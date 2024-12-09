01ea00c4bc417cdc1a2b93c1548b49cd3cfba5cc
Ghayda-Saify <g.safw2018@gmail.com>
2024-12-09 19:11:17 +0200

    initial files

---
+++ src/test/java/org/example/AcceptanceTest/AdminAdditionTest.java
@@ -0,0 +1,58 @@
+package org.example.AcceptanceTest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class AdminAdditionTest {
+
+    @Given("the admin has selected the {string} option")
+    public void the_admin_has_selected_the_option(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the instructor's name, age, specialization, email, and password are provided correctly")
+    public void the_instructor_s_name_age_specialization_email_and_password_are_provided_correctly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should display a message indicating that the instructor has been added successfully")
+    public void the_system_should_display_a_message_indicating_that_the_instructor_has_been_added_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("one or more fields \\(name, age, specialization, email, or password) are left empty")
+    public void one_or_more_fields_name_age_specialization_email_or_password_are_left_empty() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should display an error message indicating that the addition process has been rejected due to missing information")
+    public void the_system_should_display_an_error_message_indicating_that_the_addition_process_has_been_rejected_due_to_missing_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the client's name, age, email, and password are provided correctly")
+    public void the_client_s_name_age_email_and_password_are_provided_correctly() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("the system should display a message indicating that the client has been added successfully")
+    public void the_system_should_display_a_message_indicating_that_the_client_has_been_added_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("one or more fields \\(name, age, email, or password) are left empty")
+    public void one_or_more_fields_name_age_email_or_password_are_left_empty() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+}
--- src/test/java/org/example/AcceptanceTest/acceptanceTest.java
+++ src/test/java/org/example/AcceptanceTest/acceptanceTest.java
@@ -3,40 +3,19 @@ package org.example.AcceptanceTest;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/Features/Admin/add.feature",
+    plugin = {"html:target/cucumber/wikipedia.html"},
+    monochrome = true,
+    snippets = CucumberOptions.SnippetType.CAMELCASE,
+    glue = {"org.example.AcceptanceTest"}
+)
+
 
 public class acceptanceTest {
 
-
-    @Given("the admin has chosen add instructon option")
-    public void the_admin_has_chosen_add_instructon_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("instructor name , age , specialization, email and password entered correctly")
-    public void instructor_name_age_specialization_email_and_password_entered_correctly() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("the admin will get a message tells that addition process is competed successfully")
-    public void the_admin_will_get_a_message_tells_that_addition_process_is_competed_successfully() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-
-    @Given("the admin has chosen add client option")
-    public void the_admin_has_chosen_add_client_option() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @When("client name , age , email and password entered correctly")
-    public void client_name_age_email_and_password_entered_correctly() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
-
-    @When("instructor name or age or specialization or email or password was empty")
-    public void instructorNameOrAgeOrSpecializationOrEmailOrPasswordWasEmpty() {
-    }
 }
