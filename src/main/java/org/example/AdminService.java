7e2742f7d2b4a71e31138eafb3ed83c3de4dbe88
Ghayda-Saify <g.safw2018@gmail.com>
2024-12-20 13:45:53 +0200

    Admin testcode

--- src/main/java/org/example/AdminService.java
+++ src/main/java/org/example/AdminService.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public interface AdminService {
     List<Instructor> getPendingInstructors();
-
+    List<Client> getPendingClient();
     Object getUserActivityReport();
 }
