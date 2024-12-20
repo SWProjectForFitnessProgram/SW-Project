0953d03283a653dbead8bda2a34b3018c18a1729
Ghayda-Saify <g.safw2018@gmail.com>
2024-12-20 15:22:29 +0200

    Admin testcode

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -1,4 +1,34 @@
 package org.example;
 
 public class Client {
+
+    private Long id;
+
+    //    @Column(nullable = false, unique = true)
+    String email;
+
+
+    //    @Column(nullable = false)
+    String password;
+
+    //    @Enumerated(EnumType.STRING)
+    private InstructorStatus status;
+
+    public Client(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Client() {
+
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
 }
