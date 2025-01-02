67c35d3debe09522f195bf4583b29b774c3b2c36
TalaAlhendi <talaalhendiuni4@gmail.com>
2025-01-02 15:15:00 +0200

    edit the first feature with schedule details and test everything is passed

--- src/test/java/org/example/AcceptanceTest/programManagementStepTest.java
+++ src/test/java/org/example/AcceptanceTest/programManagementStepTest.java
@@ -6,7 +6,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.example.*;
-import org.junit.BeforeClass;
+
 
 
 import java.util.ArrayList;
